d6dd103680773cb97a0288ff286adbc3fcc52d78
Diego Clair <diego93rodrigues@gmail.com>
2021-09-25 23:49:08 -0300

    feat: add .env file

--- infra/data/migrations/mysql.go
+++ infra/data/migrations/mysql.go
@@ -2,7 +2,6 @@ package migrations
 
 import "github.com/GuiaBolso/darwin"
 
-//Migrations to execute our queries that changes database structure
 //Only work doing 1 command per version, you cannot create two tables in the same script, need to create a new version
 var (
 	Migrations = []darwin.Migration{
