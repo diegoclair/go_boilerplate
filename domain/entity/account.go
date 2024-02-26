188da2fc4f3e08f1ad812e3c2a1bec11e4a2576e
Diego Clair <diego93rodrigues@gmail.com>
2024-02-26 17:25:06 -0300

    chore: fix typo

--- domain/entity/account.go
+++ domain/entity/account.go
@@ -24,6 +24,6 @@ func (a *Account) SubtractBalance(amount float64) {
 	a.Balance = number.RoundFloat(a.Balance-amount, 2)
 }
 
-func (t *Account) HasSufficientFunds(amount float64) bool {
-	return t.Balance >= amount
+func (a *Account) HasSufficientFunds(amount float64) bool {
+	return a.Balance >= amount
 }
